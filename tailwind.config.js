b74b2a29a216f28053bb630f9809ccb34ce1dfa6
mmaesom <winifred.esomonu@stetis.com>
2024-03-10 22:00:53 +0000

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: "jit",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-black": {
+          DEFAULT: "#23252B",
+          100: "#23252B",
+          900: "#000000"
+        },
+        "light-grey": {
+          DEFAULT: "#747A88",
+          100: "#617587",
+          200: "#E6E9F2",
+          300: "#D4D3EB",
+        },
+        "light-white": {
+          DEFAULT: "#FFFFFF",
+          100: "#FAFAFA"
+        },
+        "light-green": {
+          DEFAULT: "#E1EDD9",
+          100: "#E1EDD9"
+        },
+        "light-red": {
+          DEFAULT: "#FADFE4",
+          100: "#FADFE4"
+        },
       },
     },
   },
