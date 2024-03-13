be292b940093cf5b7eeec235ee0c77057eb07a3c
mmaesom <winifred.esomonu@stetis.com>
2024-03-13 02:28:37 +0000

    bug fixes

--- app/collections/page.js
+++ app/collections/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { Suspense } from 'react';
 import { collections } from '@/constants/collections/Collections';
 import { useSearchParams } from 'next/navigation';
 import CollectionPage from '@/components/collections/CollectionPage';
@@ -16,11 +15,9 @@ const Page = () => {
   const selectedCollection = collections.find(collection => collection.id == itemId);
  
   return (
-    <Suspense>
-      <div>
+    <div>
       <CollectionPage selectedCollection={selectedCollection} />
-      </div>
-    </Suspense>
+    </div>
   )
 
 }
